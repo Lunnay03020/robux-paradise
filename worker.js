a6dc4a0c5227c984e04caddc8cc5eebc7921da94
Lunnay <cossmownot@gmail.com>
2025-06-08 01:53:48 +0200

    Fix

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@ const PAGES_URL = 'https://robux-paradise-aq2.pages.dev';
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    if (url.pathname.startsWith('/access/')) {
+    if (/^\/access\/[^\/]+$/.test(url.pathname)) {
       return fetch(`${PAGES_URL}/access.html`, request);
     }
     return fetch(`${PAGES_URL}${url.pathname}`, request);
