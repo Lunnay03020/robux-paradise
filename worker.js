f19c7d5d70f6c00d704255537aada1b7ab3d1b02
Lunnay <cossmownot@gmail.com>
2025-06-08 01:24:25 +0200

    fix: correct asset fetch in worker

--- worker.js
+++ worker.js
@@ -1,15 +1,9 @@
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-
-    if (url.pathname.startsWith("/access/")) {
-      const accessHtml = await env.ASSETS.fetch("https://robux-paradise.com/access.html");
-      return new Response(accessHtml.body, {
-        status: 200,
-        headers: {
-          "content-type": "text/html"
-        }
-      });
+    if (url.pathname.startsWith('/access/')) {
+      const assetRequest = new Request(`${url.origin}/access.html`, request);
+      return await env.ASSETS.fetch(assetRequest);
     }
     return env.ASSETS.fetch(request);
   }
