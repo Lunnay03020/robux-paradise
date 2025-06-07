07b3a24430ebc6e451d7390e76891375099e104c
Lunnay <cossmownot@gmail.com>
2025-06-08 01:50:26 +0200

    Fix

--- worker.js
+++ worker.js
@@ -1,15 +1,11 @@
-const PAGES_URL = 'https://robux-paradise.com';
+const PAGES_URL = 'https://robux-paradise-aq2.pages.dev';
 
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-
-    // Route dynamique vers access.html
     if (url.pathname.startsWith('/access/')) {
       return fetch(`${PAGES_URL}/access.html`, request);
     }
-
-    // Tout le reste : sert depuis ton site Pages
     return fetch(`${PAGES_URL}${url.pathname}`, request);
   }
 }
